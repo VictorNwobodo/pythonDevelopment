ea8a2b3f0bd7c40ed67753a82456967841768345
VictorNwobodo <anthonyvictoran2003@gmail.com>
2024-02-16 22:36:40 +0100

    Knowing how numbers are used in python.

---
+++ number.py
@@ -0,0 +1,16 @@
+#printing a number in python is easy.
+
+print(45)
+
+#Using a variable to print a number.
+
+number = 45
+print(number) #this method can still go with mathemacial operations like adding numbers together.
+
+num1 = 100
+num2 = 300
+genNum = num1 + num2
+print(genNum, "Is the value gotten!")
+# Here I created a variable that holds and adds up to different variable with just a single root variable called the 'genNum"
+
+
